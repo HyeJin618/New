548febbcba2290bcb42854a845bd3d1a366fffe5
DESKTOP-8L3B3L4\전혜진 <wjshj98@naver.com>
2023-12-21 23:21:50 +0900

    졸음 패널티

--- JustStudy/Assets/Resource/PlayerController.cs
+++ JustStudy/Assets/Resource/PlayerController.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rig;
-    float jumpForce = 680f; //Á¡ÇÁ
-    float walkForce = 30f;
-    float maxWalkSpeed = 2f;
+    Animator animator;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        Application.targetFrameRate = 60;
+        this.rig = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-        rig.AddForce(transform.up *  jumpForce);
+            this.rig.AddForce(transform.up * this.jumpForce);
         }
-    }
-    int key = 0;
 
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rig.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rig.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2.0f;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("°ñ");
+        SceneManager.LoadScene("ClearScene");
+    }
 }
